3e5805e377655ee6d2fc3cb59b0d5307fbd09daa
dalbay <fadalbay@gmail.com>
2019-10-02 13:25:40 -0400

    upload app files

---
+++ Master_Pages/XEx09Reservation/Site.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx09Reservation
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        private string currentYear = DateTime.Today.ToShortDateString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblYear.Text = currentYear;
+            }
+        }
+    }
+}
