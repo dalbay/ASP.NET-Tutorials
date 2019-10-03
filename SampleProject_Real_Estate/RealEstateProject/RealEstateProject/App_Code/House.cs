cc36e022e9e5bfc524b72c3984b6e8cee7138e2a
dalbay <fadalbay@gmail.com>
2019-10-03 10:10:51 -0400

    add_code file initialized

---
+++ SampleProject_Real_Estate/RealEstateProject/RealEstateProject/App_Code/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for House
+/// </summary>
+public class House
+{
+    public House()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
