ab0764bfdb48e3518d3a852deb6f6d16a0bade71
dalbay <fadalbay@gmail.com>
2019-10-04 12:27:31 -0400

    js file upload

---
+++ SampleProject_Real_Estate/RealEstateProject/RealEstateProject/Content/myScript.js
@@ -0,0 +1,16 @@
+﻿//var modal = document.getElementById("myModal").addEventListener("mouseover", function () {
+//    modal.style.display = "block";
+//});
+//var boxModals = document.querySelectorAll("svg.myModalBox").addEventListener("mouseover", function () {
+//    modal.style.display = "block";
+//});
+
+//function display() {
+//     modal.style.display = "block";
+//}
+
+document.querySelector("h1").innerHTML = "hlghfg";
+window.alert("reading it");
+
+
+
