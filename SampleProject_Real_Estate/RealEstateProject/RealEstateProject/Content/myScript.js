8a961cfb054a7276344e38493ecb521254939b89
dalbay <fadalbay@gmail.com>
2019-10-04 14:05:42 -0400

    js file modified

--- SampleProject_Real_Estate/RealEstateProject/RealEstateProject/Content/myScript.js
+++ SampleProject_Real_Estate/RealEstateProject/RealEstateProject/Content/myScript.js
@@ -1,28 +1,15 @@
-﻿var modal = document.getElementById("myModal");
+﻿//var modal = document.getElementById("myModal");
 
-var boxModals = document.querySelectorAll("svg.myModalBox");
+//var boxModals = document.querySelectorAll("svg.myModalBox");
 
-var index = 0;
-for (var i = 0; i < boxModals.length; i++) {
-    boxModals[i].addEventListener("mouseover", display);
-}
+//var index = 0;
+//for (var i = 0; i < boxModals.length; i++) {
+//    boxModals[i].addEventListener("mouseover", display);
+//}
 
-
-
-
-
-
-
-
-
-//boxModals[0].addEventListener("mouseover", display(this));
-//console.log(boxModals[0]);
-//console.log(modal);
 
 function display(x) {
 
-   // modal.style.display = "block";
-    var rect = x.getBoundingClientRect();
     var modal = document.createElement("div");
     modal.className = "modal";
     var modalContent = document.createElement("div");
@@ -31,12 +18,11 @@ function display(x) {
     modalContent.appendChild(content);
     modal.appendChild(modalContent);
 
-    var rect = x.getBoundingClientRect();
-
     var parent = x.parentElement.parentElement;
     console.log(parent);
     parent.appendChild(modal);
     modal.style.display = "block";
+
     // reset modal to display none after a short delay
     setTimeout(function() {
     modal.style.display = "none";
